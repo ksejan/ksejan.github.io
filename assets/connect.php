8fd51da698b3662063a4c522125e559ae8487613
ksejan <khaledsejan1234@gmail.com>
2022-11-17 18:02:09 +0100

    no photo + added php support

---
+++ assets/connect.php
@@ -0,0 +1,10 @@
+<?php
+    $nombre = $_POST['nombre']
+    $email = $_POST['email']
+    $asunto = $_POST['asunto']
+    $mensaje = $_POST['mensaje']
+
+    //Conectar base de datos//
+    
+    
+?>
